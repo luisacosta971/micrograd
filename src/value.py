b5a34df96b9cb8618ec3cfba5ef30dd2efbd7e83
luisacosta971 <luisacosta971@gmail.com>
2024-06-09 13:14:47 -0500

    value class

---
+++ src/value.py
@@ -0,0 +1,53 @@
+import graph_module as graph_module
+import math
+
+class Value:
+    
+    def __init__(self, data, _children=(), _op=''):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+        
+    def __repr__(self):
+        return f'Value({self.data})'
+    
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+            
+        out._backward = _backward
+        return out
+    
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+        
+        def _backward():
+            self.grad = other.data * out.grad
+            other.grad = self.data * out.grad
+            
+        out._backward = _backward
+        return out
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        out = Value(t, (self, ), 'tanh')
+        
+        def _backward():
+            self.grad = (1 - t**2) * out.grad
+            
+        out._backward = _backward
+        return out
+        
+    
+a = Value(2)
+b = Value(9)
+c = a * b 
+
+graph = graph_module.draw_dot(c)
+graph.render('output/graph', format='png', view=False)
